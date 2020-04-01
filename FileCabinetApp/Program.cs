59a949f4448df6ad7a7c1ea42548590ff45c6cbc
John Turner <johnturner.2489@gmail.com>
2020-04-01 23:32:52 +0300

    Change DeveloperName to Ivan Babitski.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -4,7 +4,7 @@ namespace FileCabinetApp
 {
     public static class Program
     {
-        private const string DeveloperName = "Vasili Vasilyev";
+        private const string DeveloperName = "Ivan Babitski";
         private const string HintMessage = "Enter your command, or enter 'help' to get help.";
         private const int CommandHelpIndex = 0;
         private const int DescriptionHelpIndex = 1;
@@ -26,7 +26,7 @@ namespace FileCabinetApp
 
         public static void Main(string[] args)
         {
-            Console.WriteLine($"File Cabinet Application, developed by {Program.DeveloperName}");
+            Console.WriteLine($"File Cabinet Application, developed by {Program.IvanBabitski}");
             Console.WriteLine(Program.HintMessage);
             Console.WriteLine();
 
