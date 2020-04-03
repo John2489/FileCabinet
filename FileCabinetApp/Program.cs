ceff3e3bb1e12e8e695c96a477043f7a808f1d7d
John Turner <johnturner.2489@gmail.com>
2020-04-03 22:17:33 +0300

    Add implementation for stat command.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -11,16 +11,19 @@ namespace FileCabinetApp
         private const int ExplanationHelpIndex = 2;
 
         private static bool isRunning = true;
+        private static FileCabinetService fileCabinetService = new FileCabinetService();
 
         private static Tuple<string, Action<string>>[] commands = new Tuple<string, Action<string>>[]
         {
             new Tuple<string, Action<string>>("help", PrintHelp),
+            new Tuple<string, Action<string>>("stat", Stat),
             new Tuple<string, Action<string>>("exit", Exit),
         };
 
         private static string[][] helpMessages = new string[][]
         {
             new string[] { "help", "prints the help screen", "The 'help' command prints the help screen." },
+            new string[] { "stat", "prints statistic about records", "The 'stat' command prints quantity of records." },
             new string[] { "exit", "exits the application", "The 'exit' command exits the application." },
         };
 
@@ -89,6 +92,12 @@ namespace FileCabinetApp
             }
 
             Console.WriteLine();
+        }
+
+        private static void Stat(string parameters)
+        {
+            var recordsCount = Program.fileCabinetService.GetStat();
+            Console.WriteLine($"{recordsCount} record(s).");
         }
 
         private static void Exit(string parameters)
